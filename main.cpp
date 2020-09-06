57a71ac82756e94ef9f27df1311972a6ffc27d4d
bcmmeijer <bcm.meijer@gmail.com>
2020-09-06 23:14:06 +0200

    using LoadLibrary's search abilities

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 #include <stdexcept>
-#include <tuple>
+#include <vector>
 #include <DbgHelp.h>
 #pragma comment(lib, "Dbghelp.lib")
 
@@ -10,33 +10,43 @@
 #define ARCH_SIG IMAGE_FILE_MACHINE_I386
 #endif
 
-bool verify_arch(const std::wstring& path) {
-	constexpr size_t	size = sizeof(IMAGE_DOS_HEADER) + sizeof(IMAGE_NT_HEADERS);
-	uint8_t				buffer[size] = { 0 };
-	HANDLE				file = nullptr;
-	DWORD				read = 0;
-	PIMAGE_DOS_HEADER	dos = nullptr;
-	PIMAGE_NT_HEADERS	nt = nullptr;
+//bool verify_arch(const std::wstring& path) {
+//
+//	constexpr size_t	size = sizeof(IMAGE_DOS_HEADER) + sizeof(IMAGE_NT_HEADERS);
+//	uint8_t				buffer[size] = { 0 };
+//	HANDLE				file = nullptr;
+//	DWORD				read = 0;
+//	PIMAGE_DOS_HEADER	dos = nullptr;
+//	PIMAGE_NT_HEADERS	nt = nullptr;
+//
+//	file = CreateFileW(path.c_str(), FILE_GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
+//	if (!file || file == INVALID_HANDLE_VALUE) throw std::runtime_error("[-] Could not open specified DLL");
+//
+//	if (!ReadFile(file, buffer, size, &read, nullptr)) {
+//		CloseHandle(file);
+//		throw std::runtime_error("[-] Could not read file");
+//	}
+//
+//	CloseHandle(file);
+//
+//	dos = reinterpret_cast<PIMAGE_DOS_HEADER>(buffer);
+//	if (dos->e_magic != IMAGE_DOS_SIGNATURE)
+//		throw std::runtime_error("[-] DOS header signature does not match expected signature");
+//
+//	nt = reinterpret_cast<PIMAGE_NT_HEADERS>((uintptr_t)dos + dos->e_lfanew);
+//	if (nt->Signature != IMAGE_NT_SIGNATURE)
+//		throw std::runtime_error("[-] NT headers signature does not match expected signature");
+//
+//	return nt->FileHeader.Machine == ARCH_SIG;
+//}
 
-	file = CreateFileW(path.c_str(), FILE_GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, nullptr);
-	if (!file || file == INVALID_HANDLE_VALUE) throw std::runtime_error("[-] Could not open specified DLL");
-
-	if (!ReadFile(file, buffer, size, &read, nullptr)) {
-		CloseHandle(file);
-		throw std::runtime_error("[-] Could not read file");
-	}
-
-	CloseHandle(file);
-
-	dos = reinterpret_cast<PIMAGE_DOS_HEADER>(buffer);
-	if (dos->e_magic != IMAGE_DOS_SIGNATURE)
-		throw std::runtime_error("[-] DOS header signature does not match expected signature");
-
-	nt = reinterpret_cast<PIMAGE_NT_HEADERS>((uintptr_t)dos + dos->e_lfanew);
-	if (nt->Signature != IMAGE_NT_SIGNATURE)
-		throw std::runtime_error("[-] NT headers signature does not match expected signature");
-
-	return nt->FileHeader.Machine == ARCH_SIG;
+inline HMODULE load_library_if_valid(const std::wstring& path) {
+	HMODULE mod = nullptr;
+	auto errormode = GetThreadErrorMode();
+	SetThreadErrorMode(errormode | SEM_FAILCRITICALERRORS, nullptr);
+	mod = LoadLibraryW(path.c_str());
+	SetThreadErrorMode(errormode, nullptr);
+	return mod;
 }
 
 FARPROC get_proc_addr(HMODULE base, const std::wstring& function, bool name_mangling = false) {
@@ -48,6 +58,10 @@ FARPROC get_proc_addr(HMODULE base, const std::wstring& function, bool name_mang
 	PIMAGE_DOS_HEADER		dos;
 	PIMAGE_NT_HEADERS		nt;
 	PIMAGE_EXPORT_DIRECTORY exports;
+
+#ifdef _DEBUG
+	std::vector<char*>		checked;
+#endif
 
 	if (function.empty()) return nullptr;
 	size_needed = WideCharToMultiByte(CP_UTF8, 0, function.c_str(), function.size(), nullptr, 0, nullptr, nullptr);
@@ -64,29 +78,42 @@ FARPROC get_proc_addr(HMODULE base, const std::wstring& function, bool name_mang
 	std::string name = buffer;
 	delete[] buffer;
 
+#define _to_va(type, obj) reinterpret_cast<type>((uint8_t*)dos + obj)
+
 	dos = reinterpret_cast<PIMAGE_DOS_HEADER>(base);
-	nt = reinterpret_cast<PIMAGE_NT_HEADERS>((uintptr_t)base + dos->e_lfanew);
+	nt = _to_va(PIMAGE_NT_HEADERS, dos->e_lfanew);
 
 	if (!nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress)
 		throw std::runtime_error("[-] IMAGE_DIRECTORY_ENTRY_EXPORT not found");
 
-	exports = reinterpret_cast<PIMAGE_EXPORT_DIRECTORY>((uint8_t*)dos + nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
-	Address = reinterpret_cast<PDWORD>((uint8_t*)dos + exports->AddressOfFunctions);
-	Name	= reinterpret_cast<PDWORD>((uint8_t*)dos + exports->AddressOfNames);
-	Ordinal = reinterpret_cast<PWORD>((uint8_t*)dos + exports->AddressOfNameOrdinals);
+	exports = _to_va(PIMAGE_EXPORT_DIRECTORY, nt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+	Address = _to_va(PDWORD, exports->AddressOfFunctions);
+	Name	= _to_va(PDWORD, exports->AddressOfNames);
+	Ordinal = _to_va(PWORD,  exports->AddressOfNameOrdinals);
 
 	for (size_t i = 0; i < exports->NumberOfNames; i++) {
-		const char* _n = (const char*)dos + Name[i];
-		FARPROC proc = reinterpret_cast<FARPROC>((uint8_t*)dos + Address[Ordinal[i]]);
+
+		auto _n = _to_va(char*, Name[i]);
+		auto _p = _to_va(FARPROC, Address[Ordinal[i]]);
+
+#ifdef _DEBUG
+		checked.emplace_back(_n);
+#endif
 
 		if (name_mangling) {
-			if (strstr(_n, name.c_str())) return proc;
+			if (strstr(_n, name.c_str())) 
+				return _p;
 		}
 		else {
 			if (!strcmp(_n, name.c_str()))
-				return proc;
+				return _p;
 		}
 	}
+
+#ifdef _DEBUG
+	for (char* f : checked) 
+		printf("%s\n", f);
+#endif
 
 	return nullptr;
 }
@@ -119,11 +146,9 @@ int wmain(int argc, wchar_t** argv) {
 			wprintf(L"[*] No function specified. Only calling DllMain\n");
 		}
 
-		if (!verify_arch(path)) 
-			throw std::runtime_error("[-] Architecture of dll does not match loader. maybe use the other loader?!");
-
-		loaded_dll = LoadLibraryW(path.c_str());
-		if (!loaded_dll) throw std::runtime_error("[-] Failed to load specified DLL");
+		loaded_dll = load_library_if_valid(path);
+		if (!loaded_dll)
+			throw std::runtime_error("[-] Failed to load DLL. Do the architectures match?");
 
 		wprintf(L"[*] DLL Loaded\n");
 
@@ -146,7 +171,7 @@ int wmain(int argc, wchar_t** argv) {
 		}
 	}
 	catch (const std::runtime_error & e) {
-		printf("[-] %s: GetLastError=%d\n", e.what(), GetLastError());
+		printf("%s: GetLastError=%d\n", e.what(), GetLastError());
 	}
 
 	return 0;
